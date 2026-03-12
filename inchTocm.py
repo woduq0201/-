b5764931423b28ba6d23de2f5a17c2135c54508e
woduq0201 <goalpe123@naver.com>
2026-03-12 22:11:41 +0900

    new file:   CircleCal.py 	modified:   inchTocm.py 	new file:   kgToPound.py

--- inchTocm.py
+++ inchTocm.py
@@ -1,3 +1,3 @@
 inch = float(input("인치값 : "))
 cm = inch * 2.54 # 1인치 = 2.54cm
-print(cm) 
+print(inch,"inch는 ",cm,"cm다",sep="") 
---
+++ kgToPound.py
@@ -0,0 +1,3 @@
+kg = float(input("kg값: "))
+pound = kg * 2.20462 # 1kg = 2.20462 파운드
+print(kg,"킬로그램은 ",pound, "파운드다",sep="")
