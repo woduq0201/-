f15aa6d887abfceedc1f3baa122915d9ce7710c2
woduq0201 <goalpe123@naver.com>
2026-03-13 00:12:50 +0900

    modified:   practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,25 @@
+a = str(input(""))
+a += "~" # a뒤에 ~붙이기
+print (a)
+a *= 2 # a를 두번반복 
+print (a) 
 
+b = int(input(""))
+b += 10
+print(b)
+b -=10
+print(b)
+b *= 2
+print(b)
+b /= 2
+print(b)
+b **= 2
+print (b)
+b %= 2
+print (b)
+
+c = str(1234)
+print(len(c))
+print(len("\""))
+
+      
