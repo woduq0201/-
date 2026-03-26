ee5397f39478a69c2fc45fd92f21c7533d807f12
woduq0201 <goalpe123@naver.com>
2026-03-26 17:52:50 +0900

    modified:   3.py

--- 3.py
+++ 3.py
@@ -7,8 +7,7 @@ if a == "1" :
 if a == "2" : 
     num1 = int(input("첫번째 숫자를 입력하세요 :"))
     num2 = int(input("두번째 숫자를 입력하세요 :"))
-    num3 = num1
+    num3 = 0
     for num in range(num1,num2+1) :
-        num3 = num1 + num
-        num4 = num1 + num3
-    print(num1,"+...+",num2,"는",num4,"입니다")
+        num3 += num
+    print(num1,"+...+",num2,"는",num3,"입니다")
