f9c760228c2bd247ac67e3ae7205af3468e37618
woduq0201 <goalpe123@naver.com>
2026-04-02 15:06:16 +0900

    modified:   5week.py

--- 5week.py
+++ 5week.py
@@ -3,5 +3,5 @@ for i in range(2,10):
 print()
 for num1 in range(1,10):
     for num2 in range(2,10):
-        print(num2,"X",num1,"=",num2*num1,"  ",end="\t")
+        print(num2,"X",num1,"=",num2*num1,end="\t")
     print()
