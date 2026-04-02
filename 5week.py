b849b4b0f5a2540e655f2d450f40ddb101834397
woduq0201 <goalpe123@naver.com>
2026-04-02 14:02:16 +0900

    modified:   5week.py

--- 5week.py
+++ 5week.py
@@ -1,7 +1,7 @@
 for i in range(2,9):
-    print("# ", i ,"단  #  ",end="")
+    print("# ", i ,"단  #  ",end="\t")
 print("# 9단 #")
 for num1 in range(1,10):
-    for num2 in range(2,10):
-        print(num2,"X",num1,"=",num2*num1,"  ",end="")
-    print(num2,"X",num1,"=",num2*(num1))
+    for num2 in range(2,9):
+        print(num2,"X",num1,"=",num2*num1,"  ",end="\t")
+    print(num2+1,"X",num1,"=",(num2+1)*num1)
