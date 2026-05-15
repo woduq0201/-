e6bc48c8eb5f695a88a1d73c04f02f488efcc2df
woduq0201 <goalpe123@naver.com>
2026-05-15 09:52:33 +0900

    new file:   "09_202610803_\354\236\204\354\236\254\354\227\275.py"

---
+++ 09_202610803_임재엽.py
@@ -0,0 +1,7 @@
+print("평균 학점을 계산해드립니다")
+math = int(input("수학 점수 입력: "))
+eng = int(input ("영어 점수 입력: "))
+kor = int(input ("국어 점수 입력: "))
+sci = int(input ("과학 점수 입력: "))
+print("평균 학점은", (math+eng+kor+sci)/4 , "입니다")
+print("4과목 점수 총합은", math+eng+kor+sci, "입니다" )
